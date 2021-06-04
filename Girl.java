20afc5164380d5f48d276827b0f2b4188906f483
lytyazla <48278370+lytyazla@users.noreply.github.com>
2021-06-04 18:30:38 +0900

    q
    q

---
+++ Girl.java
@@ -0,0 +1,31 @@
+
+public class Girl {
+
+	/** 名前 */
+	public String name;
+	/** 年齢 */
+	public int age;
+	/** 身長 */
+	public double height;
+
+	/**
+	 *
+	 * @param name
+	 * @param age
+	 * @param height
+	 */
+	public Girl(String name, int age, double height) {
+
+		this.name = name;
+		this.age = age;
+		this.height = height;
+	}
+
+	/**
+	 *
+	 */
+	public Girl() {
+
+	}
+
+}
---
+++ TestGril.java
@@ -0,0 +1,14 @@
+
+public class TestGril {
+
+	public static void main(String[] args) {
+
+		//暗然的なコンストラクタ
+		Girl g1 = new Girl();
+
+		//明⽰的なコンストラクタ
+		Girl g2 = new Girl("xin yuan", 18, 168.0);
+
+	}
+
+}
